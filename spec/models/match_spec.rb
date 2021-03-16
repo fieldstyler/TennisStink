2c30a057c11d6caac469b16b4ccb1c58c2076881
fieldstyler <tdfields93@gmail.com>
2021-03-16 11:38:11 -0600

    Add validations to match model

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Match, type: :model do
   describe 'relationships' do
     it {should belong_to(:player)}
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :type}
+    it { should validate_presence_of :score}
+    it { should validate_presence_of :result}
+    it { should validate_presence_of :date}
+    it { should validate_presence_of :surface}
+  end
 end
