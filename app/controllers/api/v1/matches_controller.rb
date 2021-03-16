0eb70b520e278294962e8d8a04220c7f92268bf9
fieldstyler <tdfields93@gmail.com>
2021-03-16 11:57:38 -0600

    Update Controllers, routes to have api,v1 namespaces

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -1,3 +1,28 @@
 class Api::V1::MatchesController < ApplicationController
+  def index
+    render json: MatchSerializer.new(Match.all)
+  end
 
+  # def show
+  #   render json: MatchSerializer.new(Match.find(params[:id]))
+  # end
+  #
+  # def create
+  #   render json: MatchSerializer.new(Match.create(match_params))
+  # end
+  #
+  # def update
+  #   render json: MatchSerializer.new(Match.update(params[:id], match_params))
+  # end
+  #
+  # def destroy
+  #   Match.destroy(params[:id])
+  #   render status: 204
+  # end
+  #
+  # private
+  #
+  # def match_params
+  #   params.permit(:type, :score, :result, :date, :surface, :notes)
+  # end
 end
