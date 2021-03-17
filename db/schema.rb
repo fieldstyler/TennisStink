4e4cf76558dcbc9d1a0f11a2c9b27583a3f862f4
fieldstyler <tdfields93@gmail.com>
2021-03-17 12:35:34 -0600

    Change all match attributes of 'type' to 'match-type' since AR doesn't allow 'type' to be the name of a column.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_051349) do
+ActiveRecord::Schema.define(version: 2021_03_17_182925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "matches", force: :cascade do |t|
-    t.string "type"
+    t.string "match_type"
     t.string "score"
     t.string "result"
     t.string "date"
