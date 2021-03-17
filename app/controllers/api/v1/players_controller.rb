3100ea8c97bc41c7e68e9bb48122444a1f098dea
fieldstyler <tdfields93@gmail.com>
2021-03-16 22:31:33 -0600

    Update controller with create action

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -6,10 +6,10 @@ class Api::V1::PlayersController < ApplicationController
   def show
     render json: PlayerSerializer.new(Player.find(params[:id]))
   end
-  #
-  # def create
-  #   render json: PlayerSerializer.new(Player.create(player_params))
-  # end
+
+  def create
+    render json: PlayerSerializer.new(Player.create(player_params))
+  end
   #
   # def update
   #   render json: PlayerSerializer.new(Player.update(params[:id], player_params))
@@ -20,9 +20,9 @@ class Api::V1::PlayersController < ApplicationController
   #   render status: 204
   # end
   #
-  # private
-  #
-  # def player_params
-  #   params.permit(:name, :ranking, :location, :username, :password)
-  # end
+  private
+
+  def player_params
+    params.permit(:name, :ranking, :location, :username, :password)
+  end
 end
