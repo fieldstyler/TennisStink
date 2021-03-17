07bdb3cfde2d19de22d9e9c191f22db9fcd812ae
fieldstyler <tdfields93@gmail.com>
2021-03-17 12:35:56 -0600

    Create passing test for find all matches

--- spec/requests/api/v1/matches/match_request_spec.rb
+++ spec/requests/api/v1/matches/match_request_spec.rb
@@ -2,29 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Matches API Request' do
   it 'imports a list of all matches' do
+    tyty = Player.create!(name: "Tyler Fields", ranking: "4.5", location: "Lone Tree, CO", username: "tytyfields", password: "12345")
+    harry = Player.create!(name: "Harry Fields", ranking: "4.5", location: "Lone Tree, CO", username: "hayhayfields", password: "abcde")
+    match1 = tyty.matches.create!(match_type: "Singles", score: "6-4, 6-0", result: "Win", date: "01-03-2021", surface: "Hard Court", notes: "Played well. Stay aggressive throughout the match.")
+    match2 = tyty.matches.create!(match_type: "Singles", score: "6-3, 7-6(3)", result: "Win", date: "02-13-2021", surface: "Clay", notes: "Bad forehand. Backswing too close to body.")
+    match3 = harry.matches.create!(match_type: "Singles", score: "2-6, 6-4, 10-8", result: "Loss", date: "10-03-2020", surface: "Hard Court", notes: "Going for too much on my shots")
+    match4 = harry.matches.create!(match_type: "Singles", score: "7-5, 6-1", result: "Win", date: "06-22-2020", surface: "Hard Court", notes: "Way too many double faults")
     get '/api/v1/matches'
 
     expect(response).to be_successful
-    require "pry"; binding.pry
-    items = JSON.parse(response.body, symbolize_names: true)
-    expect(items[:data].size).to eq(5)
-    item_hash = items[:data]
-    item_hash.each do |item|
+    matches = JSON.parse(response.body, symbolize_names: true)
+    expect(matches[:data].size).to eq(4)
+    match_hash = matches[:data]
+    match_hash.each do |match|
 
-      expect(item).to have_key(:id)
-      expect(item).to have_key(:attributes)
-      expect(item[:attributes]).to have_key(:name)
-      expect(item[:attributes][:name]).to be_a String
-      expect(item[:attributes]).to have_key(:description)
-      expect(item[:attributes][:description]).to be_a String
-      expect(item[:attributes]).to have_key(:unit_price)
-      expect(item[:attributes][:unit_price]).to be_a Float
-      expect(item[:attributes]).to have_key(:merchant_id)
-      expect(item[:attributes][:merchant_id]).to be_an Integer
-      expect(item[:attributes]).to have_key(:created_at)
-      expect(item[:attributes][:created_at]).to be_a String
-      expect(item[:attributes]).to have_key(:updated_at)
-      expect(item[:attributes][:updated_at]).to be_a String
+      expect(match).to have_key(:id)
+      expect(match).to have_key(:attributes)
+      expect(match[:attributes]).to have_key(:match_type)
+      expect(match[:attributes][:match_type]).to be_a String
+      expect(match[:attributes]).to have_key(:score)
+      expect(match[:attributes][:score]).to be_a String
+      expect(match[:attributes]).to have_key(:result)
+      expect(match[:attributes][:result]).to be_a String
+      expect(match[:attributes]).to have_key(:date)
+      expect(match[:attributes][:date]).to be_an String
+      expect(match[:attributes]).to have_key(:surface)
+      expect(match[:attributes][:surface]).to be_a String
+      expect(match[:attributes]).to have_key(:notes)
+      expect(match[:attributes][:notes]).to be_a String
     end
   end
 end
