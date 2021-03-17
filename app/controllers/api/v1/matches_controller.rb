54c60906a019962921065b0e8e7a84f666b480d8
fieldstyler <tdfields93@gmail.com>
2021-03-17 15:29:36 -0600

    Add passing tests for updating a match and add corresponding update action to match controller

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -6,23 +6,23 @@ class Api::V1::MatchesController < ApplicationController
   def show
     render json: MatchSerializer.new(Match.find(params[:id]))
   end
-  #
-  # def create
-  #   render json: MatchSerializer.new(Match.create(match_params))
-  # end
-  #
-  # def update
-  #   render json: MatchSerializer.new(Match.update(params[:id], match_params))
-  # end
+
+  def create
+    render json: MatchSerializer.new(Match.create(match_params))
+  end
+  
+  def update
+    render json: MatchSerializer.new(Match.update(params[:id], match_params))
+  end
   #
   # def destroy
   #   Match.destroy(params[:id])
   #   render status: 204
   # end
   #
-  # private
-  #
-  # def match_params
-  #   params.permit(:type, :score, :result, :date, :surface, :notes)
-  # end
+  private
+
+  def match_params
+    params.permit(:match_type, :score, :result, :date, :surface, :notes, :player_id)
+  end
 end
--- spec/requests/api/v1/matches/match_request_spec.rb
+++ spec/requests/api/v1/matches/match_request_spec.rb
@@ -63,6 +63,83 @@ RSpec.describe 'Matches API Request' do
     expect(match[:data][:attributes]).to have_key(:notes)
     expect(match[:data][:attributes][:notes]).to be_a String
   end
+  it 'can create a match record' do
+    player = Player.create!(name: "Tyler Fields", ranking: "4.5", location: "Lone Tree, CO", username: "tytyfields", password: "12345")
+
+    match_params = ({
+      match_type: "Singles",
+      score: "6-0, 6-0",
+      result: "Win",
+      date: "04-06-2020",
+      surface: "Hard Court",
+      notes: "Maybe I should go easier on my dad",
+      player_id: player.id
+      })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+    post "/api/v1/matches", headers: headers, params: JSON.generate(match_params)
+    created_match = Match.last
+    expect(response).to be_successful
+    expect(created_match.id).to be_an Integer
+    expect(created_match.match_type).to eq(match_params[:match_type])
+    expect(created_match.score).to eq(match_params[:score])
+    expect(created_match.result).to eq(match_params[:result])
+    expect(created_match.date).to eq(match_params[:date])
+    expect(created_match.surface).to eq(match_params[:surface])
+    expect(created_match.notes).to eq(match_params[:notes])
+    expect(created_match.player_id).to eq(player.id)
+  end
+
+  it 'can create match record without notes' do
+    player = Player.create!(name: "Tyler Fields", ranking: "4.5", location: "Lone Tree, CO", username: "tytyfields", password: "12345")
+
+    match_params = ({
+      match_type: "Singles",
+      score: "6-0, 6-0",
+      result: "Win",
+      date: "04-06-2020",
+      surface: "Hard Court",
+      player_id: player.id
+      })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+    post "/api/v1/matches", headers: headers, params: JSON.generate(match_params)
+    created_match = Match.last
+    expect(response).to be_successful
+    expect(created_match.notes).to eq(nil)
+  end
+
+  it 'can update a match record' do
+    tyty = Player.create!(name: "Tyler Fields", ranking: "4.5", location: "Lone Tree, CO", username: "tytyfields", password: "12345")
+    match = tyty.matches.create!(match_type: "Singles", score: "6-4, 6-0", result: "Win", date: "01-03-2021", surface: "Hard Court", notes: "Played well. Stay aggressive throughout the match.")
+
+    score = Match.last.score
+    match_params = ({score: "7-5, 6-0"})
+    headers = {"CONTENT_TYPE" => 'application/json'}
+
+    patch "/api/v1/matches/#{match.id}", headers: headers, params: JSON.generate(match_params)
+    updated_match = Match.find(match.id)
+
+    expect(response).to be_successful
+    expect(updated_match.score).to_not eq(score)
+    expect(updated_match.score).to eq("7-5, 6-0")
+  end
+
+  it 'can add notes to a match without notes' do
+    tyty = Player.create!(name: "Tyler Fields", ranking: "4.5", location: "Lone Tree, CO", username: "tytyfields", password: "12345")
+    match = tyty.matches.create!(match_type: "Singles", score: "6-4, 6-0", result: "Win", date: "01-03-2021", surface: "Hard Court")
+
+    notes = Match.last.notes
+    match_params = ({notes: "I'm pretty amazing"})
+    headers = {"CONTENT_TYPE" => 'application/json'}
+
+    patch "/api/v1/matches/#{match.id}", headers: headers, params: JSON.generate(match_params)
+    updated_match = Match.find(match.id)
+
+    expect(response).to be_successful
+    expect(updated_match.notes).to_not eq(nil)
+    expect(updated_match.notes).to eq("I'm pretty amazing")
+  end
 
   it 'returns all matches for a specific player id' do
 
